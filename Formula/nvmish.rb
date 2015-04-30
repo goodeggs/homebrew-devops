f1b88156ca308e0f5c8440eb73d157b3782bf2b0
Bob Zoller <bob@zoller.us>
2015-04-30 08:59:44 -0700

    nvmish handles new style and old style nvm directories

--- Formula/nvmish.rb
+++ Formula/nvmish.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Nvmish < Formula
   homepage 'https://github.com/goodeggs/homebrew-delivery-eng/'
   url 'https://github.com/goodeggs/homebrew-delivery-eng.git'
-  version '1.0.1'
+  version '1.0.2'
 
   depends_on 'jq'
   depends_on 'nvm'
@@ -13,22 +13,22 @@ class Nvmish < Formula
   def install
     bin.install 'nvmish'
     (bin+'nvmish').chmod 0755
+  end
 
-    ohai <<-'EOF'
-
+  def caveats
+    <<-EOS.undent
       To run nvmish automatically when changing directories:
 
       add these lines to your .zshrc:
 
-          function __nvmish() { nvmish chpwd }
+          function __nvmish() { nvmish "chpwd" }
           chpwd_functions=(${chpwd_functions[@]} "__nvmish")
 
       or these lines to your .bash_profile:
 
           # TBD
-    
-    EOF
 
+    EOS
   end
 
 end
