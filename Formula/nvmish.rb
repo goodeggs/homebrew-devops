1b001f6104ca612cc39c2e1813a9027937cf5748
Bob Zoller <bob@zoller.us>
2015-04-30 07:42:03 -0700

    adds nvmish

---
+++ Formula/nvmish.rb
@@ -0,0 +1,34 @@
+require 'formula'
+
+class Nvmish < Formula
+  homepage 'https://github.com/goodeggs/homebrew-delivery-eng/'
+  url 'https://github.com/goodeggs/homebrew-delivery-eng.git'
+  version '1.0.0'
+
+  depends_on 'jq'
+  depends_on 'nvm'
+
+  skip_clean 'bin'
+
+  def install
+    bin.install 'nvmish'
+    (bin+'nvmish').chmod 0755
+
+    ohai <<-EOF
+
+      To run nvmish automatically when changing directories:
+
+      add these lines to your .zshrc:
+
+          function __nvmish() { nvmish chpwd }
+          chpwd_functions=(${chpwd_functions[@]} "__nvmish")
+
+      or these lines to your .bash_profile:
+
+          # TBD
+    
+    EOF
+
+  end
+
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+homebrew-delivery-eng
+---------------------
+The Delivery Engineering Team's Homebrew formulae.
+
+* `nvmish` -- an rvm-like node version selector that reads engines.node from package.json
+
+----
+
+## Installation
+
+```
+brew tap goodeggs/delivery-eng
+brew install nvmish
+```
+
