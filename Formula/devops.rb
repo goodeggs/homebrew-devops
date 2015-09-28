e07b91f142960a5e635ca324edb5685427db9d5a
Bob Zoller <bob@zoller.us>
2015-09-28 16:14:56 -0700

    adds devops

---
+++ Formula/devops.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Devops < Formula
+  homepage 'https://github.com/goodeggs/homebrew-delivery-eng/'
+  url 'https://github.com/goodeggs/homebrew-delivery-eng.git'
+  version '1.0.0'
+
+  depends_on 'vault'
+  depends_on 'jq'
+
+  def install
+    bin.install 'devops'
+  end
+
+  def test
+    system "devops | grep mongo 2>&1"
+  end
+
+end
