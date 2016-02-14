7033fa5878f99e3b21edfed3cdc5b3d8f9ac0af1
Bob Zoller <bob@zoller.us>
2016-02-13 19:35:40 -0800

    ranch 1.0.2

--- Formula/ranch.rb
+++ Formula/ranch.rb
@@ -1,8 +1,8 @@
 class Ranch < Formula
   desc "Ranch Platform CLI"
   homepage "https://github.com/goodeggs/platform/tree/master/cmd/ranch"
-  url "https://github.com/goodeggs/platform/archive/v1.0.1.tar.gz"
-  sha256 "1823305347114129f241d06f4a2b2abcde71e4079475c7abf6bb0d3a8c0edf4d"
+  url "https://github.com/goodeggs/platform/archive/v1.0.2.tar.gz"
+  sha256 "1bb3db5f4cd8a8bff2639ce58dfdface37ba1eec21c2f17f3163533634a7bdd8"
 
   depends_on "go" => :build
 
