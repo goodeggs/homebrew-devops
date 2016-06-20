9eb300dd64566404b7a730a5d6a290dfed6603c8
Bob Zoller <bob@zoller.us>
2016-06-20 14:57:46 -0700

    ranch 3.0.0

--- Formula/ranch.rb
+++ Formula/ranch.rb
@@ -1,9 +1,9 @@
 class Ranch < Formula
   desc "Ranch Platform CLI"
   homepage "https://github.com/goodeggs/platform/tree/master/cmd/ranch"
-  version "2.9.1"
+  version "3.0.0"
   url "https://github.com/goodeggs/platform/releases/download/v#{version}/ranch_#{version}_darwin_amd64.zip"
-  sha256 "0cc46e48908594000fc28b286830d06a43776343ddbfa8b716843b400ad46b85"
+  sha256 "5acdc503b3d9d6784571e6a7d08b3e9f9e2e7b6730355f39721ac20aa224212c"
 
   def install
     bin.install "ranch"
