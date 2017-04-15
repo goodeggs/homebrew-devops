fd47401580f06c0a5c1967c58b649823bc0f5f5f
Bob Zoller <bob@zoller.us>
2017-04-14 15:00:41 -1000

    ranch 7.3.1

--- Formula/ranch.rb
+++ Formula/ranch.rb
@@ -1,9 +1,9 @@
 class Ranch < Formula
   desc "Ranch Platform CLI"
   homepage "https://github.com/goodeggs/platform/tree/master/cmd/ranch"
-  version "7.3.0"
+  version "7.3.1"
   url "https://github.com/goodeggs/platform/releases/download/v#{version}/ranch-Darwin-x86_64"
-  sha256 "086758e5b9cc704d74d1c55af5b8748de92d694668b546ae3b55badee00566d5"
+  sha256 "9508509a6b85c5e6541f0ab4b47430f7c3f94d2993f9876d80e2962ea4e93c0e"
 
   def install
     bin.install "ranch-Darwin-x86_64" => "ranch"
