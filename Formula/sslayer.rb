4159ea40620f4ebdcf268373871a3c77a7023c4d
Bob Zoller <bob@zoller.us>
2017-04-19 15:06:05 -1000

    sslayer

---
+++ Formula/sslayer.rb
@@ -0,0 +1,44 @@
+class Sslayer < Formula
+  desc 'SSL terminating reverse proxy for the Good Eggs ecosystem'
+  homepage 'https://github.com/goodeggs/homebrew-delivery-eng'
+  url 'https://github.com/goodeggs/homebrew-delivery-eng.git'
+
+  depends_on 'nginx'
+
+  def install
+    prefix.install 'sslayer'
+  end
+
+  def caveats
+    <<-EOS.undent
+      To complete the install of sslayer, you must run:
+        - sudo sslayer setup
+        - sudo brew services start sslayer
+    EOS
+  end
+
+  plist_options :manual => "sslayer"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <false/>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/sslayer</string>
+        </array>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
+end
