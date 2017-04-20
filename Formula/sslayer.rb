fd16ef5dba3e9594ac0dddd6dc1bcb1f1229dd7f
Bob Zoller <bob@zoller.us>
2017-04-19 15:11:50 -1000

    sslayer 1.0.2

--- Formula/sslayer.rb
+++ Formula/sslayer.rb
@@ -2,7 +2,7 @@ class Sslayer < Formula
   desc 'SSL terminating reverse proxy for the Good Eggs ecosystem'
   homepage 'https://github.com/goodeggs/homebrew-delivery-eng'
   url 'https://github.com/goodeggs/homebrew-delivery-eng.git'
-  version '1.0.1'
+  version '1.0.2'
 
   depends_on 'nginx'
 
