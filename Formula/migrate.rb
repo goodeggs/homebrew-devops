af77030498711caea28d1692985b26834b6b8f3e
Bob Zoller <bob@zoller.us>
2017-06-01 15:28:07 -1000

    add formula for migrate

---
+++ Formula/migrate.rb
@@ -0,0 +1,16 @@
+class Migrate < Formula
+  desc "Database migrations. CLI and Golang library."
+  homepage "https://github.com/mattes/migrate"
+  version "3.0.1"
+  url "https://github.com/mattes/migrate/releases/download/#{version}/migrate.darwin-amd64.tar.gz"
+  sha256 "17e19678e930d4a48114937fb07300c107088d86a80c798630fabc08d32ee4ee"
+
+  def install
+    bin.install "migrate.darwin-amd64" => "migrate"
+  end
+
+  test do
+    output = shell_output(bin/"migrate --version")
+    assert_match version, output
+  end
+end
