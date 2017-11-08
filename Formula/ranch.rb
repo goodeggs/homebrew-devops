4c84ab19adfca5dd6ae4fdaf271ae400569936c5
Keith Swett <swettk@gmail.com>
2017-11-08 20:41:36 +0000

    ranch version bump

--- Formula/ranch.rb
+++ Formula/ranch.rb
@@ -1,9 +1,9 @@
 class Ranch < Formula
   desc "Ranch Platform CLI"
   homepage "https://github.com/goodeggs/platform/tree/master/cmd/ranch"
-  version "9.0.0"
+  version "9.0.1"
   url "https://github.com/goodeggs/platform/releases/download/v#{version}/ranch-Darwin-x86_64"
-  sha256 "7c45df7bf2d9a68b3135f03e71b938b3e4fccf44db8ad8a7771f3b8287d3a40e"
+  sha256 "3dd0f031dd33b43909c61a872ee4c983c22b0a854129786a596acba515035c15"
 
   def install
     bin.install "ranch-Darwin-x86_64" => "ranch"
