46d6e400b94eb512de1dc1e95c1dd166801e485b
Bob Zoller <bob@zoller.us>
2018-04-10 08:25:44 -1000

    use GitHubPrivateRepositoryReleaseDownloadStrategy for goodeggs-cli

--- Formula/goodeggs.rb
+++ Formula/goodeggs.rb
@@ -2,7 +2,7 @@ class Goodeggs < Formula
   desc "Good Eggs CLI"
   homepage "https://github.com/goodeggs/goodeggs-cli"
   version "1.0.1"
-  url "https://github.com/goodeggs/goodeggs-cli/releases/download/v#{version}/goodeggs-Darwin-x86_64"
+  url "https://github.com/goodeggs/goodeggs-cli/releases/download/v#{version}/goodeggs-Darwin-x86_64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "d0db4fd03c0eb0e013bb754a5099cef13db9b99f4c85f6b846af440ae8e0b180"
 
   def install
