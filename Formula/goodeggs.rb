0f3b68f08a2632c586ea2d2f9d7937ce24e4b445
Bob Zoller <bob@zoller.us>
2018-04-25 10:57:38 -1000

    goodeggs-cli v1.0.3

--- Formula/goodeggs.rb
+++ Formula/goodeggs.rb
@@ -1,9 +1,9 @@
 class Goodeggs < Formula
   desc "Good Eggs CLI"
   homepage "https://github.com/goodeggs/goodeggs-cli"
-  version "1.0.2"
+  version "1.0.3"
   url "https://github.com/goodeggs/goodeggs-cli/releases/download/v#{version}/goodeggs-Darwin-x86_64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "9aebe04603852d7b39403d72881210b04ac64844ef58159a2afddfb52754e156"
+  sha256 "6d9e37f8879c238e4018cab352569e66f03ac40009895d17e694a788feaf6fed"
 
   def install
     bin.install "goodeggs-Darwin-x86_64" => "goodeggs"
