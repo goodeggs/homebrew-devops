e7cd8e6d8b320d5d1c898ebc0545bc28de9a64fc
Bob Zoller <bob@zoller.us>
2019-01-08 12:26:28 -1000

    chromedriver-service

---
+++ Formula/chromedriver-service.rb
@@ -0,0 +1,38 @@
+class ChromedriverService < Formula
+  desc "Adds a service for Chromedriver"
+  homepage "https://sites.google.com/a/chromium.org/chromedriver/"
+  version "1.0"
+
+  depends_on cask: 'chromedriver'
+
+  bottle :unneeded
+
+  plist_options :manual => "chromedriver"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>homebrew.mxcl.chromedriver</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <false/>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/chromedriver</string>
+      </array>
+      <key>ServiceDescription</key>
+      <string>Chrome Driver</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/chromedriver-error.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/chromedriver-output.log</string>
+    </dict>
+    </plist>
+    EOS
+  end
+
+end
