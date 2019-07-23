28155ccc04e51a5aef3d3858d773e54011c228b7
Bob Zoller <bob@zoller.us>
2019-07-22 21:40:15 -1000

    ranch 9.10.0
    privileged

--- Formula/ranch.rb
+++ Formula/ranch.rb
@@ -1,9 +1,9 @@
 class Ranch < Formula
   desc "Ranch Platform CLI"
   homepage "https://github.com/goodeggs/platform/tree/master/cmd/ranch"
-  version "9.9.0"
+  version "9.10.0"
   url "https://github.com/goodeggs/platform/releases/download/v#{version}/ranch-Darwin-x86_64"
-  sha256 "c8dc18768e4acd1d7360e6776e2d04b268fcd37116864626f363b37e4b14025e"
+  sha256 "594e7cf3dc8adb18c6a62e5558ba5a6ce29fd6ed49d8f02c2a9d6f676a105dac<Paste>"
 
   def install
     bin.install "ranch-Darwin-x86_64" => "ranch"
