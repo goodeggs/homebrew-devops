4e8837162e5c21b3c4af4db0498bbd5cb2de6a30
Bob Zoller <bob@zoller.us>
2019-07-24 09:21:33 -1000

    fix ranch

--- Formula/ranch.rb
+++ Formula/ranch.rb
@@ -2,11 +2,11 @@ class Ranch < Formula
   desc "Ranch Platform CLI"
   homepage "https://github.com/goodeggs/platform/tree/master/cmd/ranch"
   version "9.10.0"
-  url "https://github.com/goodeggs/platform/releases/download/v#{version}/ranch-Darwin-x86_64"
-  sha256 "594e7cf3dc8adb18c6a62e5558ba5a6ce29fd6ed49d8f02c2a9d6f676a105dac<Paste>"
+  url "http://ranch-updates.goodeggs.com/stable/ranch/#{version}/darwin-amd64.gz"
+  sha256 "148acfe2c4a1ca6dc4e8cb813f0ff485b4414f6b30b9bac40fc7c7663ed51aed"
 
   def install
-    bin.install "ranch-Darwin-x86_64" => "ranch"
+    bin.install "darwin-amd64" => "ranch"
   end
 
   test do
